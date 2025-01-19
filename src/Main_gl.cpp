dee0747cd08aa2ef6602b1e96bf78b1d3ac81fb6
djhAeon <duncanjamesharris@gmail.com>
2025-01-19 11:47:15 +0000

    Update Main_gl.cpp

--- src/Main_gl.cpp
+++ src/Main_gl.cpp
@@ -189,10 +189,10 @@ void CVisualizationWaveForm::Render()
   EnableShader();
 
   // Left channel
-  DrawLine(m_fWaveform[0], false);
+  DrawLine(m_fWaveform[0], true);
 
   // Right channel
-  // DrawLine(m_fWaveform[1], true);
+  DrawLine(m_fWaveform[1], true);
 
   DisableShader();
 
@@ -267,7 +267,7 @@ void CVisualizationWaveForm::AudioData(const float* pAudioData, size_t iAudioDat
     for (size_t i=0; i < iAudioDataLength; i+=usedStep)
     {
       m_fWaveform[0][ipos] = pAudioData[i  ]; // left channel
-      // m_fWaveform[1][ipos] = pAudioData[i+1]; // right channel
+      m_fWaveform[1][ipos] = pAudioData[i+1]; // right channel
       ipos++;
       if (ipos >= m_usedLinePoints)
          break;
