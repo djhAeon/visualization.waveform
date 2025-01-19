e64681e4307209fecc94160be9c8a933685c6c75
djhAeon <duncanjamesharris@gmail.com>
2025-01-19 13:35:04 +0000

    Update Main_gl.cpp

--- src/Main_gl.cpp
+++ src/Main_gl.cpp
@@ -255,7 +255,7 @@ void CVisualizationWaveForm::AudioData(const float* pAudioData, size_t iAudioDat
   size_t usedStep;
   if (m_ignoreResample)
   {
-    usedStep = 2;
+    usedStep = 1;
   }
   else
   {
